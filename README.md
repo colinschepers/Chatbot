c9272a6bb36a107312660f92b7605cf6173496aa
Colin Schepers <colinschepers@hotmail.com>
2020-04-17 11:12:56 +0200

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Chatbot
-
-Almost human; a React/Python based chatbot
-
-## Sources
-
-1. https://medium.com/@bhavaniravi/learn-reactjs-by-building-a-chat-frontend-2d8fe664276e
---
+++ precache-manifest.301cac9930eb0bf49140abf400db7be3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de3de174eb3295f2b0acfd1739ce597",
+    "url": "/EasyChatbot/index.html"
+  },
+  {
+    "revision": "840081e35deeee65e73e",
+    "url": "/EasyChatbot/static/css/main.271a1eba.chunk.css"
+  },
+  {
+    "revision": "f1f9247f91e4dd1b19a0",
+    "url": "/EasyChatbot/static/js/2.100ee79d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/EasyChatbot/static/js/2.100ee79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840081e35deeee65e73e",
+    "url": "/EasyChatbot/static/js/main.f36b767a.chunk.js"
+  },
+  {
+    "revision": "e0a190284740df50eb49",
+    "url": "/EasyChatbot/static/js/runtime-main.a8fab14e.js"
+  }
+]);
